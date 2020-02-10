cac8bf385f7e81de30521ede5dbd79168401cfb2
Kartheek Solipuram <kartheek.s@gmail.com>
2020-02-09 21:00:55 -0800

    process clean up for build to run compile and set up of zk snark circuits

---
+++ ares-api/src/init.js
@@ -0,0 +1,49 @@
+import compile from 'circom';
+import fs from 'fs';
+import { promisify } from 'util';
+import path from 'path';
+import zkSnark from 'snarkjs';
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const mkdir = promisify(fs.mkdir);
+const exists = promisify(fs.exists);
+const filePath = process.argv[2];
+const fileName = filePath.endsWith('.circom')
+  ? filePath.slice(0, -7).replace('circuits/', '')
+  : filePath;
+
+const main = async fileInput => {
+  try {
+    if (!exists(`${fileInput}`)) {
+      mkdir(`${fileInput}`);
+    }
+    let compiledCircuit = await compile(
+      path.join(process.cwd(), 'circuits', `${fileInput}.circom`),
+      {},
+    );
+    await writeFile(
+      path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}.json`),
+      JSON.stringify(compiledCircuit, null, 2),
+    );
+    console.log('🏗️  zkSnark circuit', `${fileInput}`, 'compiled!');
+
+    compiledCircuit = JSON.parse(await readFile(path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}.json`)), 'utf-8');
+    const circuitInstance = new zkSnark.Circuit(compiledCircuit);
+    const circuitSetup = await zkSnark.kimleeoh.setup(circuitInstance);
+    const circuitStrSetup = zkSnark.stringifyBigInts(circuitSetup);
+    await writeFile(
+        path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}_vk_proof.json`),
+        JSON.stringify(circuitStrSetup.vk_proof, null, 2),
+    );
+    await writeFile(
+        path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}_vk_verifier.json`),
+        JSON.stringify(circuitStrSetup.vk_verifier, null, 2),
+    );
+    console.log('🔩  zkSnark circuit', `${fileInput}`, 'setup using Kimleeoh proving scheme!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main(fileName);
---
+++ ares-api/src/setup.js
@@ -0,0 +1,27 @@
+import zkSnark from 'snarkjs';
+import fs from 'fs';
+import { promisify } from 'util';
+import path from 'path';
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const fileInput = process.argv[2];
+
+// To use this file for set up, run: docker-compose exec ares-api node dist/setup.js circuit_name
+const main = async() => {
+    const compiledCircuit = JSON.parse(await readFile(path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}.json`)), 'utf-8');
+    const circuitInstance = new zkSnark.Circuit(compiledCircuit);
+    const circuitSetup = await zkSnark.kimleeoh.setup(circuitInstance);
+    const circuitStrSetup = zkSnark.stringifyBigInts(circuitSetup);
+    await writeFile(
+        path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}_vk_proof.json`),
+        JSON.stringify(circuitStrSetup.vk_proof, null, 2),
+    );
+    await writeFile(
+        path.join(process.cwd(), 'circuits', 'artifacts', `${fileInput}_vk_verifier.json`),
+        JSON.stringify(circuitStrSetup.vk_verifier, null, 2),
+    );
+    console.log("🔩 Set up is done!");
+}
+
+main();
