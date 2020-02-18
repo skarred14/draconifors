e636c18a81957af2d6970bec558062675d2e351d
Kartheek Solipuram <kartheek.s@gmail.com>
2020-02-18 02:25:45 -0800

    fixes to get the routes working

--- ares-api/src/app.js
+++ ares-api/src/app.js
@@ -3,21 +3,24 @@ import cors from '@koa/cors';
 import mount from 'koa-mount';
 import bodyParser from'koa-bodyparser';
 import poseidon from './routes/poseidon_hashing';
+import healthcheck from './routes/healthcheck';
+import '@babel/polyfill';
 
 
 const main = async () => {
-    const app = new Koa()
+    const app = new Koa();
 
     // Parse incoming requests data
-    app.use(bodyParser())
+    app.use(bodyParser());
 
     // Allow CORS headers
     app.use(cors({
         credentials: true
-    }))
+    }));
 
-    app.use(mount('/poseidon',await poseidon()))
-    return app
+    app.use(mount('/healthcheck', await healthcheck()));
+    app.use(mount('/poseidon',await poseidon()));
+    return app;
 }
 
 if (require.main === module) {
---
+++ ares-api/src/routes/healthcheck.js
@@ -0,0 +1,29 @@
+import Koa from'koa'
+import route from 'koa-route'
+import '@babel/polyfill'
+
+module.exports = async () => {
+    const app = new Koa();
+    app.use(route.get('/',  async (ctx) => {
+
+        try {
+            console.log("All systems go!");
+        } catch (err) {
+            ctx.status = 500
+            ctx.body = {
+                data: {
+                    err: err
+                }
+            }
+        }
+        finally{
+            ctx.status = 200
+            ctx.body = {
+                data: {
+                    result: 'ok'
+                }
+            }
+        }
+    }));
+    return app;
+};
